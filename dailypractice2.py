1db3eb0d1782295fadf870163204901c61f20277
mayank prashar <sujanian02@gmail.com>
2019-06-13 17:25:09 +0530

    june13

---
+++ dailypractice2.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on %(date)s
+
+@author: %(mayankprashar)s
+"""
+
+list1 = [1,2,3,4,5]
+list1
+list2 =['a','b','c','c','d','e']
+list2
+list3=['hello','how','are','you']
+list3[0]
+   
+
+tuple1 =(1,2,'h','h','a','b')
+tuple1
+tuple1[0]=bla
+tuple2=(1,2,6,'fd','hello')
+tuple2
+dict1={1:'ram',2:'shyam',3:'are',4:'brother',1:'sita'}
+dict1
+set1 = set(['india','afganistan','china','srilanka'])
+set1
+str1='hello guys'
+str1
+
