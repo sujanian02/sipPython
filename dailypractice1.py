8fbd2e04cd614780e5d6dcb18aded011baef3019
mayank prashar <sujanian02@gmail.com>
2019-06-13 15:42:18 +0530

    jun13

--- dailypractice1.py
+++ dailypractice1.py
@@ -7,3 +7,15 @@ Created on %(date)s
 
 list1 = [1,2,3,4,5]
 list1
+list2 =['a','b','c','d','e']
+list2
+list3=['hello','how','are','you']
+list3[0]
+   
+
+tuple1 =(1,2,'h','a','b')
+tuple1[0]=bla
+tuple2=(1,2,6,'fd','hello')
+tuple2
+dict1={1:'ram',2:'shyam',3:'are',4:'brother'}
+dict1
